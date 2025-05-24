31af2cb42010e84f24c04f8cf59584be1a5a9fc2
ahthisisnotwhatiwant <01l1l1l0@naver.com>
2025-05-24 21:14:46 +0900

    Update Confirmation_of_Scheduled_Residence_Transfer.py

--- Confirmation_of_Scheduled_Residence_Transfer.py
+++ Confirmation_of_Scheduled_Residence_Transfer.py
@@ -86,7 +86,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # 사용자 안내
-st.markdown('<div class="instruction-message">----------  목  적  ---------- <br> 신설학교 학급 편성을 위한 정보 수집<br>----------  순  서  ---------- <br> ①지역 및 학교 → ②개인정보 수집·이용 동의서 → ③전입예정확인서 → ④제출</div>', unsafe_allow_html=True)
+st.markdown('<div class="instruction-message">🗃️ 순  서 🗃️ <br> ①지역 및 학교 → ②개인정보 수집·이용 동의서 → ③전입예정확인서 → ④제출</div>', unsafe_allow_html=True)
 
 # Streamlit Session State 초기화
 if 'stage' not in st.session_state:
