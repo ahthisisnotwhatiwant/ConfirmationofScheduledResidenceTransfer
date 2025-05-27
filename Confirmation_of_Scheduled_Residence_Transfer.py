a4215707de0ca477b5816e4ac25cc05a7dfa46d6
ahthisisnotwhatiwant <01l1l1l0@naver.com>
2025-05-27 21:48:43 +0900

    Update Confirmation_of_Scheduled_Residence_Transfer.py

--- Confirmation_of_Scheduled_Residence_Transfer.py
+++ Confirmation_of_Scheduled_Residence_Transfer.py
@@ -367,12 +367,12 @@ elif st.session_state.stage == 3:
             draw2 = ImageDraw.Draw(page2)
 
             consent_positions = {
-                "{{date.today}}": [(1000, 1550)],
-                "{{student_name}}": [(825, 1695)],
-                "{{student_sign_path}}": [(1060, 1695)],
+                "{{date.today}}": [(1100, 1550)],
+                "{{student_name}}": [(825, 1700)],
+                "{{student_sign_path}}": [(1060, 1665)],
                 "{{parent_name}}": [(825, 1820)],
-                "{{parent_sign_path}}": [(1060, 1820)],
-                "{{school_name}}": [(940, 1990)],
+                "{{parent_sign_path}}": [(1060, 1790)],
+                "{{school_name}}": [(935, 1990)],
             }
             transfer_positions = {
                 "{{student_name}}": [(480, 457), (805, 1760)],
