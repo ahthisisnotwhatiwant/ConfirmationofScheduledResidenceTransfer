2cfb394c7b5bfcf3fc14a7aed82dc998000934a5
ahthisisnotwhatiwant <01l1l1l0@naver.com>
2025-05-29 01:36:43 +0900

    Update Confirmation_of_Scheduled_Residence_Transfer.py

--- Confirmation_of_Scheduled_Residence_Transfer.py
+++ Confirmation_of_Scheduled_Residence_Transfer.py
@@ -318,7 +318,7 @@ elif st.session_state.stage == 3:
             "전학 예정 학년",
             options=["1학년", "2학년", "3학년", "4학년", "5학년", "6학년"],
             index=None,
-            placeholder="학년을 선택하세요",
+            placeholder="학년을 선택하세요.",
             key="next_grade_input"
         )
 
